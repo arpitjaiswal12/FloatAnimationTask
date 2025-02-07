1a6e3f28687590755d1aeeee843d8620d6ea1083
Arpit Jaiswal <jaiswalarpit419@gmail.com>
2025-02-07 11:31:56 +0530

    Initial Commit

---
+++ FloatAnimationTask/FloatAnimationTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatAnimationTaskApp.swift
+//  FloatAnimationTask
+//
+//  Created by Arpit Jaiswal on 07/02/25.
+//
+
+import SwiftUI
+
+@main
+struct FloatAnimationTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
